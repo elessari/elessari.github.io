93bd103ffea2c03a3c25026ce5ffacc6eb4cf1ed
elessari <sierraskorupski@outlook.com>
2020-03-02 14:07:33 -0500

    Update site landing page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,16 @@ var header;
 var span;
 var floral_p = " ☙";
 var i = 0;
+var j = 0;
 var speed = 50;
 var text = "Hey, I'm Sierra, a UX Designer.";
+var ptext = "I make pretty UI and stuff.";
 
 function loadHeader() {
     header = document.getElementById("tagline");
+    document.getElementById("projects-div").style.display = "none";
+    document.getElementById("about-div").style.display = "none";
+    headerp = document.getElementById("floral");
 
     if(i < text.length) {
         header.innerHTML += text.charAt(i);
@@ -19,9 +24,28 @@ function loadHeader() {
 }
 
 function loadFlower() {
-    span = document.getElementById("floral");
+    headerp.style.opacity = "1";
+}
 
-    
-    span.innerHTML += floral_p;
-    span.style.opacity = "1";
+
+function changeDiv() {
+    var projects = document.getElementById("projects-link");
+    var about = document.getElementById("about-link");
+    console.log(event.target);
+
+    if(event.target == projects) {
+        document.getElementById("projects-div").style.display = "block";
+        document.getElementById("about-div").style.display = "none";
+        document.getElementById("tag").style.display = "none";
+    }
+    else if(event.target == about) {
+        document.getElementById("projects-div").style.display = "none";
+        document.getElementById("about-div").style.display = "block";
+        document.getElementById("tag").style.display = "none";
+    }
+    else {
+        document.getElementById("projects-div").style.display = "none";
+        document.getElementById("about-div").style.display = "none";
+        document.getElementById("tag").style.display = "block";
+    }
 }
